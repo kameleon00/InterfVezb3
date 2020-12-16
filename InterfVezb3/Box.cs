0cfb9a4c8f00aa1ced25e4a7367af190ef9dda58
Marko Djokic <marko1319@its.edu.rs>
2020-12-16 19:03:48 +0100

    Add project files.

---
+++ InterfVezb3/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfVezb3
+{
+    class Box : IDimensions
+    {
+        float lengthInches;
+        float widthInches;
+
+        public Box (float lenght, float width)
+        {
+            lengthInches = lenght;
+            widthInches = width;
+        }
+
+        //1. Eksplicitna implementacija metode getLength:
+        float IDimensions.getLength()
+        {
+            return lengthInches;
+        }
+        float IDimensions.getWidth()
+        {
+            return widthInches;
+        }
+    }
+}
---
+++ InterfVezb3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfVezb3
+{
+    class Program
+    {
+        static void Main()
+        {
+            //1. Deklaracija instance klase Box: box1
+            Box box1 = new Box(30, 20);
+
+            //2.Deklaracija instance interfejsa IDimensions:
+            IDimensions dimensions = (IDimensions)box1;
+
+            //6. Prikaz dimenzija kutije preko instance interfejsa:
+            Console.WriteLine("Lenght {0} ", dimensions.getLength());
+            Console.WriteLine("Width {0} ", dimensions.getWidth());
+
+            Console.ReadKey();
+        }
+    }
+}
